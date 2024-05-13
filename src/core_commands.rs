3ee288b527573948fd2b3bdc65ac421b3bc23377
Daniel Duque <daniel.duque@cern.ch>
2024-05-13 02:23:01 +0200

    Get simplest run info page loading

--- src/core_commands.rs
+++ src/core_commands.rs
@@ -1,7 +1,7 @@
 use tokio::process::Command;
 
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
-enum CoreCommand {
+pub(crate) enum CoreCommand {
     ChronoboxTimestamps,
     InitialOdb,
     FinalOdb,
@@ -11,7 +11,7 @@ enum CoreCommand {
 }
 
 impl CoreCommand {
-    fn command(self) -> Command {
+    pub(crate) fn command(self) -> Command {
         match self {
             CoreCommand::ChronoboxTimestamps => Command::new("alpha-g-chronobox-timestamps"),
             CoreCommand::InitialOdb => Command::new("alpha-g-odb"),
@@ -23,6 +23,17 @@ impl CoreCommand {
             CoreCommand::Sequencer => Command::new("alpha-g-sequencer"),
             CoreCommand::TrgScalers => Command::new("alpha-g-trg-scalers"),
             CoreCommand::Vertices => Command::new("alpha-g-vertices"),
+        }
+    }
+
+    pub(crate) fn output(self, run_number: u32) -> String {
+        match self {
+            CoreCommand::ChronoboxTimestamps => format!("R{run_number}_chronobox_timestamps.csv"),
+            CoreCommand::InitialOdb => format!("R{run_number}_initial_odb.json"),
+            CoreCommand::FinalOdb => format!("R{run_number}_final_odb.json"),
+            CoreCommand::Sequencer => format!("R{run_number}_sequencer.csv"),
+            CoreCommand::TrgScalers => format!("R{run_number}_trg_scalers.csv"),
+            CoreCommand::Vertices => format!("R{run_number}_vertices.csv"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use anyhow::{Context, Result};
+use anyhow::Context;
 use askama_axum::Template;
-use axum::{extract, routing::get, Router};
+use axum::extract::{self, State};
+use axum::response::{IntoResponse, Response};
+use axum::{http::StatusCode, routing::get, Router};
 use clap::Parser;
-use std::sync::OnceLock;
+use core_commands::CoreCommand;
+use serde_json::Value;
+use std::collections::HashMap;
+use std::sync::{Arc, OnceLock};
+use tokio::{fs, process::Child};
 
 mod core_commands;
 
@@ -19,8 +25,34 @@ struct Args {
     data_dir: std::path::PathBuf,
 }
 
+#[derive(Default)]
+struct AppState {
+    childs: tokio::sync::Mutex<HashMap<(u32, CoreCommand), Child>>,
+}
+
+struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Error: {:?}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
     MIDAS_DATA_PATH
         .set(args.data_dir)
@@ -32,11 +64,11 @@ async fn main() -> Result<()> {
         .set(project_dirs.cache_dir().to_path_buf())
         .expect("failed to set CACHE_PATH");
 
-    println!("Cache path: {}", CACHE_PATH.get().unwrap().display());
-
+    let app_state = Arc::new(AppState::default());
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/:run_number", get(run_info));
+        .route("/:run_number", get(run_info))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind(args.addr)
         .await
@@ -53,10 +85,88 @@ async fn main() -> Result<()> {
 struct RunInfoTemplate {
     run_number: u32,
     start_time: String,
-    end_time: String,
+    stop_time: String,
     operator_comment: String,
 }
 
-async fn run_info(extract::Path(run_number): extract::Path<u32>) -> RunInfoTemplate {
-    todo!()
+async fn run_info(
+    State(app_state): State<Arc<AppState>>,
+    extract::Path(run_number): extract::Path<u32>,
+) -> Result<RunInfoTemplate, AppError> {
+    let contents = get_core_output(CoreCommand::FinalOdb, run_number, app_state)
+        .await
+        .with_context(|| format!("failed to get final ODB for run number `{run_number}`"))?;
+
+    let start_index = contents.iter().position(|&c| c == b'{').with_context(|| {
+        format!("failed to find JSON data in final ODB for run number `{run_number}`")
+    })?;
+    let odb = serde_json::from_slice::<Value>(&contents[start_index..])
+        .with_context(|| format!("failed to parse final ODB for run number `{run_number}`"))?;
+
+    Ok(RunInfoTemplate {
+        run_number,
+        start_time: odb
+            .pointer("/Runinfo/Start time")
+            .and_then(Value::as_str)
+            .with_context(|| format!("failed to get start time for run number `{run_number}`"))?
+            .to_string(),
+        stop_time: odb
+            .pointer("/Runinfo/Stop time")
+            .and_then(Value::as_str)
+            .with_context(|| format!("failed to get stop time for run number `{run_number}`"))?
+            .to_string(),
+        operator_comment: odb
+            .pointer("/Experiment/Edit on start/Comment")
+            .and_then(Value::as_str)
+            .with_context(|| format!("failed to get comment for run number `{run_number}`"))?
+            .to_string(),
+    })
+}
+
+async fn get_core_output(
+    command: CoreCommand,
+    run_number: u32,
+    app_state: Arc<AppState>,
+) -> Result<Vec<u8>, anyhow::Error> {
+    let cache_path = CACHE_PATH.get().unwrap().join(run_number.to_string());
+    let output_path = cache_path.join(command.output(run_number));
+
+    fs::create_dir_all(&cache_path).await.with_context(|| {
+        format!(
+            "failed to create cache directory `{}`",
+            cache_path.display()
+        )
+    })?;
+
+    match fs::read(&output_path).await {
+        Ok(contents) => Ok(contents),
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => Err(e).context("failed here"),
+        Err(e) => Err(e).with_context(|| format!("failed to read `{}`", output_path.display())),
+    }
+}
+
+async fn get_run_files(run_number: u32) -> Result<Vec<std::path::PathBuf>, anyhow::Error> {
+    let prefix = format!("run{run_number:05}sub");
+
+    let mut run_files = Vec::new();
+    let mut entries = fs::read_dir(MIDAS_DATA_PATH.get().unwrap())
+        .await
+        .context("failed to read MIDAS data directory")?;
+    while let Some(entry) = entries
+        .next_entry()
+        .await
+        .context("failed to iterate over MIDAS data directory")?
+    {
+        let file_name = entry.file_name();
+        if file_name
+            .to_str()
+            .with_context(|| format!("non-UTF-8 filename `{:?}`", entry.file_name()))?
+            .starts_with(&prefix)
+        {
+            run_files.push(entry.path());
+        }
+    }
+    run_files.sort_unstable();
+
+    Ok(run_files)
 }
