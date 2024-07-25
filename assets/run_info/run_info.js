aa6d62a3f84f69a5afb10c094fdef2fa9efa1e18
Daniel Duque <daniel.duque@cern.ch>
2024-07-24 17:28:22 -0700

    Rewrite frontend

--- assets/run_info/run_info.js
+++ assets/run_info/run_info.js
@@ -1,8 +1,18 @@
-document.querySelectorAll(".dropdown-menu a").forEach(function (element) {
-  let nextEl = element.nextElementSibling;
-  if (nextEl && nextEl.classList.contains("sub-menu")) {
+document
+  .getElementById("searchInput")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      const input = document.getElementById("searchInput").value;
+      if (input) {
+        window.location.href = "./" + encodeURIComponent(input);
+      }
+    }
+  });
+
+document.querySelectorAll(".dropdown-menu button").forEach(function (element) {
+  const nextElement = element.nextElementSibling;
+  if (nextElement && nextElement.classList.contains("sub-menu")) {
     element.addEventListener("click", function (e) {
-      // prevent opening link if link needs to open dropdown
       e.preventDefault();
       e.stopPropagation();
     });
@@ -14,13 +24,26 @@ const protocol = loc.protocol === "https:" ? "wss:" : "ws:";
 const ws = new WebSocket(protocol + "//" + loc.host + loc.pathname + "/../ws");
 
 ws.onopen = function () {
-  button = document.getElementById("downloadButton");
-  button.removeAttribute("disabled");
+  document.getElementById("downloadButton").disabled = false;
 };
 
+// The WebSocket connection is closed when the page is unloaded. In this case,
+// we don't want to show an alert.
+let isPageUnloading = false;
+window.addEventListener("beforeunload", function () {
+  isPageUnloading = true;
+});
+
 ws.onclose = function () {
-  button = document.getElementById("downloadButton");
-  button.setAttribute("disabled", "disabled");
+  document.getElementById("downloadButton").disabled = true;
+  if (
+    !isPageUnloading &&
+    !alert(
+      "Connection to the server was lost.\n\nThe page will reload when you close this alert. Please request any data or plots you were waiting for again.",
+    )
+  ) {
+    window.location.reload();
+  }
 };
 
 ws.onmessage = function (event) {
@@ -28,146 +51,203 @@ ws.onmessage = function (event) {
 
   switch (msg.service) {
     case "Download":
-      const outputId = msg.context;
-      var output = document.getElementById(outputId);
-      if (output === null) {
-        break;
-      }
-
-      if (msg.response.Text) {
-        output.textContent += msg.response.Text + "\n";
-      } else if (msg.response.Error) {
-        output.textContent += msg.response.Error + "\n";
-      } else if (msg.response.DownloadJWT) {
-        var a = document.createElement("a");
-        a.href = "./download/" + msg.response.DownloadJWT;
-        a.setAttribute("download", "");
-        a.click();
-
-        output.textContent += "Done\n";
-      }
-
+      handleDownloadResponse(msg);
       break;
   }
 };
 
-function loadUrl() {
-  const searchInput = document.getElementById("search-input").value;
-  if (searchInput) {
-    window.location.href = "./" + encodeURIComponent(searchInput);
+function handleDownloadResponse(msg) {
+  const output = document.getElementById(msg.context);
+  if (output === null) {
+    return;
+  }
+
+  if (msg.response.Text) {
+    output.textContent += msg.response.Text + "\n";
+  } else if (msg.response.Error) {
+    output.textContent += msg.response.Error + "\n";
+  } else if (msg.response.DownloadJWT) {
+    const a = document.createElement("a");
+    a.href = "./download/" + msg.response.DownloadJWT;
+    a.setAttribute("download", "");
+    a.click();
+
+    output.textContent += "Done\n";
   }
 }
-
-document.getElementById("load-button").addEventListener("click", loadUrl);
-document
-  .getElementById("search-input")
-  .addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      loadUrl();
-    }
-  });
 
 let tabCounter = 1;
 
-function createNewTab(jsonMessage) {
-  var tabId = "tab-" + tabCounter;
-  var tabContentId = "content-" + tabId;
+function newDownload(jsonMessage) {
+  const newTabId = "tab-" + tabCounter;
+  const newTabContentId = "tabContent-" + tabCounter;
+  const newTabContentOutputId = "tabContentOutput-" + tabCounter;
 
-  var tabList = document.getElementById("commandTabs");
-  var tabContent = document.getElementById("commandTabContent");
+  document.getElementById("downloadTabs").innerHTML += `
+    <li class="nav-item" id="${newTabId}">
+      <div
+        class="nav-link pe-2"
+        data-bs-toggle="tab"
+        data-bs-target="#${newTabContentId}"
+        role="button"
+      >
+        ${tabCounter}
+        <button
+          type="button"
+          class="btn-close tab-btn-close align-middle ms-1"
+          onclick="
+            document.getElementById('${newTabId}').remove();
+            document.getElementById('${newTabContentId}').remove();
 
-  var newTab = document.createElement("li");
-  newTab.className = "nav-item";
-  newTab.innerHTML = `
-		<a class="nav-link d-flex align-items-center" id="${tabId}"	data-bs-toggle="tab" href="#${tabContentId}" role="tab">
-			${tabCounter} 
-			<button class="btn-close ms-2" aria-label="Close" onclick="closeTab(event, '${tabId}', '${tabContentId}')"></button>
-		</a>`;
-  tabList.appendChild(newTab);
+            const tabs = document.querySelectorAll('#downloadTabs .nav-link');
+            if (tabs.length > 0) {
+              new bootstrap.Tab(tabs[0]).show();
+            }
+          "
+        ></button>
+      </div>
+    </li>
+  `;
 
-  var outputId = "output-" + tabContentId;
-  var newTabContent = document.createElement("div");
-  newTabContent.className = "tab-pane fade";
-  newTabContent.id = tabContentId;
-  newTabContent.setAttribute("role", "tabpanel");
-  newTabContent.innerHTML = `<pre class="p-3 bg-light border" id="${outputId}"></pre>`;
-  tabContent.appendChild(newTabContent);
+  document.getElementById("downloadTabsContent").innerHTML += `
+    <div class="tab-pane fade" id="${newTabContentId}" tabindex="0">
+      <pre
+        class="p-3 bg-light border border-top-0"
+        id="${newTabContentOutputId}"
+      ></pre>
+    </div>
+  `;
 
-  var tabTrigger = new bootstrap.Tab(document.getElementById(`${tabId}`));
-  tabTrigger.show();
+  new bootstrap.Tab(
+    document.getElementById(newTabId).querySelector(".nav-link"),
+  ).show();
+
+  jsonMessage.context = newTabContentOutputId;
+  ws.send(JSON.stringify(jsonMessage));
 
   tabCounter++;
-
-  jsonMessage.context = outputId;
-  ws.send(JSON.stringify(jsonMessage));
 }
 
-function closeTab(event, tabId, tabContentId) {
-  event.stopPropagation();
-  document.getElementById(tabId).parentElement.remove();
-  document.getElementById(tabContentId).remove();
+function updateTrgMaxTimeReq() {
+  document.getElementById("trgMaxT").min =
+    document.getElementById("trgMinT").value;
+}
 
-  var remainingTabs = document.querySelectorAll("#commandTabs .nav-link");
-  if (remainingTabs.length > 0) {
-    var firstTab = new bootstrap.Tab(remainingTabs[0]);
-    firstTab.show();
+function updateTrgScalersReq() {
+  const checkboxes = Array.from(
+    document.getElementsByName("trgScalerCheckbox"),
+  );
+
+  if (checkboxes.every((checkbox) => !checkbox.checked)) {
+    checkboxes.forEach((checkbox) => (checkbox.required = true));
+  } else {
+    checkboxes.forEach((checkbox) => (checkbox.required = false));
   }
 }
 
-function requestTrgScalersPlot() {
-  const trg_args = {
-    t_bins: parseInt(document.getElementById("TRGtBins").value),
-    t_max: parseFloat(document.getElementById("TRGtMax").value),
-    t_min: parseFloat(document.getElementById("TRGtMin").value),
-    include_drift_veto: document.getElementById("TRGDriftVeto").checked,
-    include_pulser: document.getElementById("TRGPulser").checked,
-    include_scaledown: document.getElementById("TRGScaleDown").checked,
-    remove_input: !document.getElementById("TRGInput").checked,
-    remove_output: !document.getElementById("TRGOutput").checked,
-  };
-
-  createNewTab({
-    service: "Download",
-    context: "",
-    request: { TrgScalersPlot: { run_number: var_run_number, args: trg_args } },
-  });
+function updateTrgDownloadBtnState() {
+  document.getElementById("trgDownloadBtn").disabled = !document
+    .getElementById("trgForm")
+    .checkValidity();
 }
 
-function requestVtxPlot() {
-  const vtx_args = {
-    phi_bins: parseInt(document.getElementById("phi_bins").value),
-    phi_max: parseFloat(document.getElementById("phi_max").value),
-    phi_min: parseFloat(document.getElementById("phi_min").value),
-    r_bins: parseInt(document.getElementById("r_bins").value),
-    r_max: parseFloat(document.getElementById("r_max").value),
-    r_min: parseFloat(document.getElementById("r_min").value),
-    t_bins: parseInt(document.getElementById("t_bins").value),
-    t_max: parseFloat(document.getElementById("t_max").value),
-    t_min: parseFloat(document.getElementById("t_min").value),
-    z_bins: parseInt(document.getElementById("z_bins").value),
-    z_max: parseFloat(document.getElementById("z_max").value),
-    z_min: parseFloat(document.getElementById("z_min").value),
+document
+  .querySelectorAll("#trgForm input, #trgForm select, #trgForm textarea")
+  .forEach(function (element) {
+    element.addEventListener("input", updateTrgDownloadBtnState);
+  });
+
+document
+  .getElementById("trgModal")
+  .addEventListener("show.bs.modal", function () {
+    updateTrgMaxTimeReq();
+    updateTrgScalersReq();
+    updateTrgDownloadBtnState();
+  });
+
+function trgPlot() {
+  updateTrgMaxTimeReq();
+  updateTrgScalersReq();
+
+  if (!document.getElementById("trgForm").checkValidity()) {
+    alert("Please fix all form errors before submitting.");
+    return;
+  }
+
+  const trgArgs = {
+    t_bins: parseInt(document.getElementById("trgBins").value),
+    t_max: parseFloat(document.getElementById("trgMaxT").value),
+    t_min: parseFloat(document.getElementById("trgMinT").value),
+    include_drift_veto: document.getElementById("trgDriftVeto").checked,
+    include_pulser: document.getElementById("trgPulser").checked,
+    include_scaledown: document.getElementById("trgScaledown").checked,
+    remove_input: !document.getElementById("trgInput").checked,
+    remove_output: !document.getElementById("trgOutput").checked,
   };
 
-  createNewTab({
+  newDownload({
     service: "Download",
     context: "",
-    request: { VerticesPlot: { run_number: var_run_number, args: vtx_args } },
+    request: { TrgScalersPlot: { run_number: RUN_NUMBER, args: trgArgs } },
   });
+
+  bootstrap.Modal.getInstance(document.getElementById("trgModal")).hide();
 }
 
-function requestChronoboxPlot() {
-  const cb_args = {
-    board_name: document.getElementById("boardName").value,
-    channel_number: parseInt(document.getElementById("channelNumber").value),
-    t_bins: parseInt(document.getElementById("cb_tBins").value),
-    t_max: parseFloat(document.getElementById("cb_tMax").value),
-    t_min: parseFloat(document.getElementById("cb_tMin").value),
+function updateVerticesMaxTimeReq() {
+  document.getElementById("verticesMaxT").min =
+    document.getElementById("verticesMinT").value;
+}
+
+function updateVerticesDownloadBtnState() {
+  document.getElementById("verticesDownloadBtn").disabled = !document
+    .getElementById("verticesForm")
+    .checkValidity();
+}
+
+document
+  .querySelectorAll(
+    "#verticesForm input, #verticesForm select, #verticesForm textarea",
+  )
+  .forEach(function (element) {
+    element.addEventListener("input", updateVerticesDownloadBtnState);
+  });
+
+document
+  .getElementById("verticesModal")
+  .addEventListener("show.bs.modal", function () {
+    updateVerticesMaxTimeReq();
+    updateVerticesDownloadBtnState();
+  });
+
+function verticesPlot() {
+  updateVerticesMaxTimeReq();
+
+  if (!document.getElementById("verticesForm").checkValidity()) {
+    alert("Please fix all form errors before submitting.");
+    return;
+  }
+
+  const verticesArgs = {
+    phi_bins: parseInt(document.getElementById("verticesBinsPhi").value),
+    phi_max: parseFloat(document.getElementById("verticesMaxPhi").value),
+    phi_min: parseFloat(document.getElementById("verticesMinPhi").value),
+    r_bins: parseInt(document.getElementById("verticesBinsR").value),
+    r_max: parseFloat(document.getElementById("verticesMaxR").value),
+    r_min: parseFloat(document.getElementById("verticesMinR").value),
+    t_bins: parseInt(document.getElementById("verticesBinsT").value),
+    t_max: parseFloat(document.getElementById("verticesMaxT").value),
+    t_min: parseFloat(document.getElementById("verticesMinT").value),
+    z_bins: parseInt(document.getElementById("verticesBinsZ").value),
+    z_max: parseFloat(document.getElementById("verticesMaxZ").value),
+    z_min: parseFloat(document.getElementById("verticesMinZ").value),
   };
 
-  createNewTab({
+  newDownload({
     service: "Download",
     context: "",
-    request: { ChronoboxPlot: { run_number: var_run_number, args: cb_args } },
+    request: { VerticesPlot: { run_number: RUN_NUMBER, args: verticesArgs } },
   });
+
+  bootstrap.Modal.getInstance(document.getElementById("verticesModal")).hide();
 }
